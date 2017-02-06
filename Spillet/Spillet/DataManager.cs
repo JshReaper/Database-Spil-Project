82e7ddf211a2c08db438bb27adb8abd671ecdb5f
Jacob Hansen <jshchansen@gmail.com>
2017-02-06 14:32:49 +0100

    added a datamanager class and added code learnt last week.

---
+++ Spillet/Spillet/DataManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+namespace Spillet
+{
+    static class DataManager
+    {
+        static void Manage()
+        {
+            bool fileExists = false;
+            if (!File.Exists("Data.db"))
+            {
+                SQLiteConnection.CreateFile("Data.db");
+                SQLiteConnection dbConnOnCreate = new SQLiteConnection("Data Source=Data.db;Version=3;");
+                dbConnOnCreate.Open();
+                //initial logic
+                String sqlOnCreate = "create table highscores (id integer primary key,name varchar(20), score int)";
+                SQLiteCommand commandOnCreate = new SQLiteCommand(sqlOnCreate, dbConnOnCreate);
+                commandOnCreate.ExecuteNonQuery();
+
+                //end logic
+                dbConnOnCreate.Close();
+                
+                fileExists = true;
+            }
+            if (File.Exists("Data.db") && !fileExists)
+            {
+                //message to user?
+            }
+
+            SQLiteConnection dbConn = new SQLiteConnection("Data Source=Data.db;Version=3;");
+
+            dbConn.Open();
+            //ongoing logic
+            
+            //end logic
+           
+        }
+    }
+}

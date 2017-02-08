c7e264b46a6b2237fa59c4d29580954739cf901e
Jacob Hansen <jshchansen@gmail.com>
2017-02-08 13:42:19 +0100

    a

--- Spillet/Spillet/GameWorld.cs
+++ Spillet/Spillet/GameWorld.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,31 @@ namespace Spillet
 {
     class GameWorld
     {
+        private static List<GameObject> gameObjects = new List<GameObject>();
+        public static List<GameObject> GameObjects { get { return gameObjects; } }
 
+        public GameWorld(Graphics dc, Rectangle displayRectangle)
+        {
+            SetupWorld();
+        }
+        
+        void SetupWorld()
+        {
+            //add objects and so on which should be there on load
+        }
+
+        public void GameLoop()
+        {
+            
+        }
+        public void Draw()
+        {
+            
+        }
+
+        public void Update(float fps)
+        {
+            
+        }
     }
 }

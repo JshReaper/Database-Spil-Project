5c5ea94d53b5ab60310e69a020278acd5429d6e2
Vicissitude1 <d-madsen@live.dk>
2017-02-08 13:58:51 +0100

    Sup?

--- Spillet/Spillet/GameObject.cs
+++ Spillet/Spillet/GameObject.cs
@@ -2,10 +2,36 @@
 {
    abstract class GameObject
    {
-       private Vector2D position;
+        private bool alive;
+        private Vector2D position;
+        protected Rectangle displayRectangle;
+        private float animationSpeed;
+        private float scaleFactor;
+        private float currentFrameIndex;
+        private List<Image> animationFrame;
+        protected Rectangle spritePart;
+
         public GameObject()
         {
 
+        }
+        //Alive
+        public bool Alive
+        {
+            get
+            {
+                return alive;
+            }
+            set
+            {
+                alive = value;
+            }
+        }
+        //Henter og sætter værdigen af position
+        protected Vector2D Posistion
+        {
+            get { return position; }
+            set { position = value; }
         }
 
 

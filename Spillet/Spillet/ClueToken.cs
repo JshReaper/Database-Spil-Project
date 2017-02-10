b79cbd80db023fba53afa6d34efe97e3d6255d03
Vicissitude1 <d-madsen@live.dk>
2017-02-10 14:13:44 +0100

    Add viseble hitbox

--- Spillet/Spillet/ClueToken.cs
+++ Spillet/Spillet/ClueToken.cs
@@ -4,6 +4,7 @@ namespace Spillet
     {
         public ClueToken(float speed, string imgPath, Vector2D pos, float scaleFactor, float animationSpeed) : base(speed, imgPath, pos, scaleFactor, animationSpeed)
         {
+
         }
 
         public override void OnCollision(GameObject other)

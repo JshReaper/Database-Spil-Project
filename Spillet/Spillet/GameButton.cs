22ea7cef8dcdbfdb943b7c6375fa3fb325455465
Jacob Hansen <jshchansen@gmail.com>
2017-02-15 11:22:13 +0100

    sfas

---
+++ Spillet/Spillet/GameButton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spillet
+{
+    class GameButton: GameObject
+    {
+        public GameButton(float speed, string imgPath, Vector2D pos, float scaleFactor, float animationSpeed) : base(speed, imgPath, pos, scaleFactor, animationSpeed)
+        {
+        }
+    }
+}
